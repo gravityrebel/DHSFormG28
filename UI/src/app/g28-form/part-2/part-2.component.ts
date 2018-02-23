117fd0452da8d82914421d129bda368e64e3e5f5
Daniel Kim <danielckim05@gmail.com>
2018-02-23 14:41:40 -0500

    Added required fields and state dropdown

--- UI/src/app/g28-form/part-2/part-2.component.ts
+++ UI/src/app/g28-form/part-2/part-2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-part-2',
@@ -6,9 +7,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./part-2.component.css']
 })
 export class Part2Component implements OnInit {
-
+  
+  lastName = '';
+  firstName = '';
+  stNumberName = '';
+  states: string[] = ["AK",
+            "AL",
+            "AR",
+            "AS",
+            "AZ",
+            "CA",
+            "CO",
+            "CT",
+            "DC",
+            "DE",
+            "FL",
+            "GA",
+            "GU",
+            "HI",
+            "IA",
+            "ID",
+            "IL",
+            "IN",
+            "KS",
+            "KY",
+            "LA",
+            "MA",
+            "MD",
+            "ME",
+            "MI",
+            "MN",
+            "MO",
+            "MS",
+            "MT",
+            "NC",
+            "ND",
+            "NE",
+            "NH",
+            "NJ",
+            "NM",
+            "NV",
+            "NY",
+            "OH",
+            "OK",
+            "OR",
+            "PA",
+            "PR",
+            "RI",
+            "SC",
+            "SD",
+            "TN",
+            "TX",
+            "UT",
+            "VA",
+            "VI",
+            "VT",
+            "WA",
+            "WI",
+            "WV",
+            "WY"];
+  zip='';
   constructor() { }
-
+  savePart2() {
+  
+  }
   ngOnInit() {
   }
 
